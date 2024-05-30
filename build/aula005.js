cc0c301b29f7bce93c0d2f554a2b8ebec1a4d92f
Cleverson Andrade <cleversonandrade62@gmail.com>
2024-05-30 16:20:22 -0300

    curso_ts_a5_p1

---
+++ build/aula005.js
@@ -0,0 +1,7 @@
+"use strict";
+let cursos = ['JavaScript', 'React', 'TypeScript'];
+let valores = [10, 5, 3];
+cursos.push('Git');
+valores.push(0, 2, 7);
+console.log(cursos);
+console.log(valores);
---
+++ src/aula005.ts
@@ -0,0 +1,8 @@
+let cursos: string[] = ['JavaScript', 'React', 'TypeScript'];
+let valores: number[] = [10, 5, 3];
+
+cursos.push('Git');
+valores.push(0, 2, 7);
+
+console.log(cursos);
+console.log(valores);
