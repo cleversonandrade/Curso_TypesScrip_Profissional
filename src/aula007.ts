8d00182775382d1ec7bb61a71ba8c5ec74935b60
Cleverson Andrade <cleversonandrade62@gmail.com>
2024-06-01 19:57:23 -0300

    curso_ts_a7

---
+++ src/aula007.ts
@@ -0,0 +1,10 @@
+/*let coisas: (number | string)[] = [10, 20, 'string'];
+console.log(coisas); */
+
+let coisas:[string, number, Number, boolean] = ['casa', 200, 300, false];
+
+coisas.push('kit médico', 5, true);
+
+coisas[3] = true;
+console.log(coisas);
+console.log(coisas[3]);
