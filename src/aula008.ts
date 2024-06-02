7526c033319955eba082ff0be884d441ab9f0390
Cleverson Andrade <cleversonandrade62@gmail.com>
2024-06-02 20:37:55 -0300

    curso_ts_a8

---
+++ src/aula008.ts
@@ -0,0 +1,21 @@
+let funcionario = {
+    nome: 'Raimundo',
+    setor: 'Tráfego',
+    status:'Ativo',
+    data_val: 2024,
+    ola:() => {console.log('Oi')},
+    info:(p: string) => {console.log(p)}
+}
+
+funcionario.nome = 'Gilson';
+funcionario.status = 'Inativo';
+funcionario.setor = 'Gestão';
+
+funcionario.ola();
+funcionario.info('Cleverson');
+
+console.log(typeof(funcionario));
+console.log(funcionario.nome);
+console.log(funcionario.setor);
+console.log(funcionario.status);
+console.log(funcionario.data_val);
